9f47687bb8d87166aadd699c080fabab64d08798
anhdoursa <anh.do@ursa.vn>
2022-04-05 11:59:35 +0700

    jsconfig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import HomePage from 'components/HomePage/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
-import HomePage from './components/HomePage/HomePage';
-import ShopPage from './components/ShopPage/ShopPage';
 import CartPage from './components/CartPage/CartPage';
+import Header from './components/Header/Header';
+import ShopPage from './components/ShopPage/ShopPage';
 import Register from './features/Register/Register';
 function App() {
   return (
